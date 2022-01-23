9b71b76f66774bf9b4c387ff0e413ee88b9eff69
MamathaBobbili <61958206+MamathaBobbili@users.noreply.github.com>
2022-01-23 14:20:25 +0530

    Time: 17 ms (44.81%), Space: 36.3 MB (81.69%) - LeetHub

---
+++ 37-sudoku-solver/37-sudoku-solver.java
@@ -0,0 +1,43 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+    
+        if(board==null || board.length==0)
+            return;
+        solve(board);
+    }
+    public static boolean solve(char[][] board){
+        for(int i=0;i<board.length;i++)
+        {
+            for(int j=0;j<board[0].length;j++){
+
+                if(board[i][j]=='.')
+                {
+                    for(char c='1';c<='9';c++){
+                        if(isSafe(board,i,j,c))
+                        {
+                            board[i][j]=c;    
+                            if(solve(board))
+                                return true;
+                            else{
+                                board[i][j]='.';
+                            }
+                        }
+                    }
+                    return false;
+                }
+                
+            }
+        }
+        return true;
+    }
+    
+    public static boolean isSafe(char[][]board,int row,int col,char val)
+    {
+        for(int i=0;i<9;i++){
+           if(board[row][i] != '.' && board[row][i]==val) return false;
+           if(board[i][col] != '.' && board[i][col]==val) return false;
+           if(board[3 * (row / 3) + i / 3][ 3 * (col / 3) + i % 3] != '.' && board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == val) return false;
+        }
+        return true;
+    }
+}
