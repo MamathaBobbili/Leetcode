53ca8db5fe3622e5d626408662f9c07ce85872fc
MamathaBobbili <61958206+MamathaBobbili@users.noreply.github.com>
2022-01-26 13:29:07 +0530

    Time: 3 ms (44.29%), Space: 38.8 MB (24.15%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -0,0 +1,50 @@
+class Solution {
+    public boolean isOpenBrace(char c){
+	        char[] openBraces={'{','[','('};
+	        for(char item:openBraces)
+	        {
+	        	if(item==c)
+	        	{
+	        		 return true;
+	        	}
+	        }
+	           
+	        return false;
+	    }
+	    public boolean isValid(String s) {
+	        
+	         char ch[] = s.toCharArray();
+	        Stack<Character> stk = new Stack<>();
+	        Map<Character,Character>  map = new HashMap<>();
+	        boolean balanced=false;
+	        map.put('{','}');
+	        map.put('[',']');
+	        map.put('(',')');
+	        for(int c=0;c<ch.length;c++)
+	        {
+	        	if(isOpenBrace(ch[c])){
+	                    stk.push(ch[c]);
+	                    balanced= false;
+	                }
+	                else{
+	                	if(stk.isEmpty())
+	                		return false;
+	                	else{
+		                    char popchar = stk.pop();
+		                    if(map.containsKey(popchar)){
+		                        char val = map.get(popchar);
+		                        if(val==ch[c])
+		                        {
+		                        	balanced= true;
+                                    if(c==ch.length-1 && !stk.isEmpty())
+		                        		return false;
+		                        }
+		                        else
+		                        	return false;
+		                    }
+	                	}
+	                }
+	            }         
+	        return balanced;
+	    }
+}
