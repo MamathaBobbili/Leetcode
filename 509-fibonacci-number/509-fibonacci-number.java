bb028098a5f40ca1abb10a4e48cb77df4613b515
MamathaBobbili <61958206+MamathaBobbili@users.noreply.github.com>
2022-01-28 16:16:23 +0530

    Time: 14 ms (12.17%), Space: 40.8 MB (6.84%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -3,12 +3,12 @@ class Solution {
     Start from the given number and to base case
     **/ 
     
-    // public int fib(int n) {
-    //     if(n<=1)
-    //         return n;
-    //     else
-    //         return fib(n-1)+fib(n-2);
-    // }
+    public int fib(int n) {
+        if(n<=1)
+            return n;
+        else
+            return fib(n-1)+fib(n-2);
+    }
     
     /** Tabulation: Bottom Up approach
         start from 0 and go to n
@@ -26,21 +26,21 @@ class Solution {
         Rather than using array , we are trying with two pointers
     **/
     
-     public int fib(int n) {
-        int prev2=0;
-        int prev1=1;
+//      public int fib(int n) {
+//         int prev2=0;
+//         int prev1=1;
          
-        if(n==0)
-         return prev2;
-        if(n==1)
-            return prev1;
+//         if(n==0)
+//          return prev2;
+//         if(n==1)
+//             return prev1;
         
-        int curr=0;
-        for(int i=2;i<=n;i++){
-            curr=prev1+prev2;
-            prev2=prev1;
-            prev1=curr;
-        }
-        return curr;
-    }
+//         int curr=0;
+//         for(int i=2;i<=n;i++){
+//             curr=prev1+prev2;
+//             prev2=prev1;
+//             prev1=curr;
+//         }
+//         return curr;
+//     }
 }
