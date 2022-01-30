e852f571d3e91efe8e9f0a142a3dd1b37e39bcc8
MamathaBobbili <61958206+MamathaBobbili@users.noreply.github.com>
2022-01-30 18:54:53 +0530

    Time: 1 ms (26.14%), Space: 42.4 MB (5.34%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.java
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -1,16 +1,28 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         	 
-		  if(nums2.length>0){			 
+     /*** Append 2nd array and sort 
+		 int k=0;
+		 for(int i=0;i<nums1.length;i++){
+			 if(nums1[i]==0){
+				 nums1[i]=nums2[k];
+				 k++;
+			 }
+		 }
+		 Arrays.sort(nums1);
+     **/
+        
+     /** In place sorting**/ 
+	    if(nums2.length>0){			 
 		
 		 for(int i=0;i<nums1.length;i++)
 		 {
-			 if(nums1[i]>nums2[0])
+			 if(nums1[i]>nums2[0])//compare first value of 2nd array if greater then swap
 			 {
 				 int t=nums2[0];
 				 nums2[0]=nums1[i];
 				 nums1[i]=t;
-				 //Arrays.sort(nums2);
+                 /**sort 2nd array after swapping**/
 				 int f = nums2[0];
 				 int l;
 				 for(l=1;l<nums2.length && f>nums2[l];l++)
@@ -20,6 +32,7 @@ class Solution {
 				 nums2[l-1]=f;
 			 }			 
 		 }
+            /** place last elements in first array**/
 		 int k=nums2.length-1;
 		 for(int i =nums1.length-1 ;i>=0 && k>=0;i--){
 			 if(nums1[i]==0){
