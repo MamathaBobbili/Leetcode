a7e66904faaeb74da9661813f0eda3765acbebe7
MamathaBobbili <61958206+MamathaBobbili@users.noreply.github.com>
2022-02-09 21:01:21 +0530

    Time: 19 ms (33.48%), Space: 48.7 MB (8.89%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.java
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.java
@@ -3,7 +3,7 @@ class Solution {
         int count=0;
 		
         Map<Integer,Integer> map= new HashMap<Integer,Integer>();
-        for(int i=0;i<nums.length;i++)
+        for(int i=0;i<nums.length;i++) //maintain frequency of each value
           {
         	map.put(nums[i], map.getOrDefault(nums[i],0)+1);
           }
@@ -11,16 +11,44 @@ class Solution {
         {
         	if(k>0)
         	{
-        		int sum=i+k;
+                /* add the k value to eachkey and check result exist in the map*/
+        		int sum=i+k;        
         		if(map.containsKey(sum))
         			count++;
         	}
         	else if (k==0)
         	{
+                /* check eachkey whose frequency greaterthan 1*/
         		if(map.get(i)>1)
         			count++;
         	}
         }
         return count;
+        
+       /*
+       Below approach is failing in because keys and values are getting duplicated
+       for(int i=0;i<nums.length;i++)
+       {
+           for(int j=i+1;j<nums.length;j++)
+           {
+               if(Math.abs(nums[i]-nums[j])==k)
+               {
+                   System.out.println(nums[j]+"---"+nums[i]);
+                   if(map.containsKey(nums[i])){
+                   	if(map.get(nums[i])==nums[j]){
+                   		continue;
+                   	}
+                   	map.put(nums[j],nums[i]);
+                   }
+                   else{
+                   	map.put(nums[j],nums[i]);
+
+                   }
+               }
+           }
+       }
+        System.out.println(map.toString());
+        return map.size();
+        */
     }
 }
