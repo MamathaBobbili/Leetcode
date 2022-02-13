c6ef55e9a0ead8930994178b63d784dbe872d0cc
MamathaBobbili <61958206+MamathaBobbili@users.noreply.github.com>
2022-02-13 07:56:19 +0530

    Time: 1 ms (77.93%), Space: 43.9 MB (6.35%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -4,18 +4,27 @@ class Solution {
         List<List<Integer>> res = new ArrayList<List<Integer>>();
         List<Integer> ans = new ArrayList<Integer>();
         getSubsets(0,nums,ans,res);
-        //System.out.println(res.toString());
         return res;
     }
     
     public void getSubsets(int index,int[] nums,List<Integer> ans,List<List<Integer>> res)
     {
-    	res.add(new ArrayList<Integer>(ans));
-    	for(int i=index;i<nums.length;i++){
+    	res.add(new ArrayList<Integer>(ans));//add each result
+    	for(int i=index;i<nums.length;i++){ //iterate every index
        		ans.add(nums[i]);
 	        getSubsets(i+1,nums,ans,res);
 	        ans.remove(ans.size()-1);
     	}
         
     }
+    /** Recursion Tree 
+    
+             []      1 2 3
+                    /  |  \
+              [1]1   [2]2  3[3] 
+             /\        | 
+       [1,2]2  3[1,3]  3[2,3] 
+           /
+           3[1,2,3]
+          **/
 }
